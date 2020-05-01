e3cea23b3e00aadb0a24fc412767cfcb786db773
Aniket1298 <tekina1298@gmail.com>
2020-05-01 11:34:21 +0530

    CP-python template

---
+++ cp.py
@@ -0,0 +1,15 @@
+from math import ceil,sqrt
+from fractions import gcd
+from sys import stdin,stdout
+def l():
+	return map(int,stdin.readline().split())
+def i():
+	return int(stdin.readline())
+def s():
+	return stdin.readline()
+def ol(l):
+	for i in l:
+		stdout.write(str(i)+' ')
+	stdout.write('\n')
+def oi(n):
+	stdout.write(str(n))
